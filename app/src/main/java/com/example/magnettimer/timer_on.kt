4889d2431d2ba6fd0c3583a3d9bccf02bfc18c95
Taeshmello <sdh220305@sdh.hs.kr>
2023-11-22 23:28:59 +0900

    almost done

---
+++ app/src/main/java/com/example/magnettimer/timer_on.kt
@@ -0,0 +1,14 @@
+package com.example.magnettimer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class timer_on : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timer_on)
+
+
+
+    }
+}
