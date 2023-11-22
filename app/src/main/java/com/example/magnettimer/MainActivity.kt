a3338d5d41c85c755b9ed93ff5e74fb14bb8cc98
kimminam <bluemind8520@gmail.com>
2023-11-22 22:01:27 +0900

    modified NFC code

--- app/src/main/java/com/example/magnettimer/MainActivity.kt
+++ app/src/main/java/com/example/magnettimer/MainActivity.kt
@@ -1,63 +1,62 @@
+package com.example.magnettimer
+
 import android.app.PendingIntent
 import android.content.Intent
-import android.content.IntentFilter
-import android.nfc.NdefMessage
 import android.nfc.NfcAdapter
-import android.nfc.Tag
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-//    private var nfcAdapter: NfcAdapter? = null
-//    private var nfcPendingIntent: PendingIntent? = null
-//    private val writeTagFilters = arrayOf(IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED))
+    private var nfcAdapter: NfcAdapter? = null
+    private var nfcPendingIntent: PendingIntent? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//
-//        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
-//
-//        val intent = Intent(this, javaClass).apply {
-//            addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
-//        }
-//        nfcPendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
-//    }
-//
-//    override fun onResume() {
-//        super.onResume()
-//        nfcAdapter?.enableForegroundDispatch(this, nfcPendingIntent, null, null)
-//    }
-//
-//    override fun onPause() {
-//        super.onPause()
-//        nfcAdapter?.disableForegroundDispatch(this)
-//    }
-//
-//    override fun onNewIntent(intent: Intent) {
-//        super.onNewIntent(intent)
-//        handleIntent(intent)
-//    }
-//
-//    private fun handleIntent(intent: Intent) {
-//        if (NfcAdapter.ACTION_NDEF_DISCOVERED == intent.action) {
-//            intent.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG)?.let { tag ->
-//                val messages = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
-//                if (messages != null) {
-//                    for (message in messages) {
-//                        val records = (message as NdefMessage).records
-//                        for (record in records) {
-//                            val payload = record.payload
-//                            val text = String(payload, Charsets.UTF_8)
-//                            // 읽은 데이터를 처리합니다.
-//                            val textView = findViewById<TextView>(R.id.textView)
-//                            textView.text = text
-//                        }
-//                    }
-//                }
-//            }
-//        }
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+
+        val intent = Intent(this, javaClass)
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        nfcPendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        // 버튼 클릭 이벤트 처리
+        val startButton = findViewById<Button>(R.id.Timestart)
+        startButton.setOnClickListener {
+            // NFC 기다림
+            nfcAdapter?.enableForegroundDispatch(this, nfcPendingIntent, null, null)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (nfcAdapter != null) {
+            nfcAdapter!!.enableForegroundDispatch(this, nfcPendingIntent, null, null)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (nfcAdapter != null) {
+            nfcAdapter!!.disableForegroundDispatch(this)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        handleIntent(intent)
+    }
+
+    private fun handleIntent(intent: Intent) {
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED == intent.action) {
+            showToast("NFC가 작동되었습니다.")
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
