42a4e3351367a4df13da509cd3ea6d1e9db864be
maldron0309 <bagjinhyeong640@gmail.com>
2023-12-01 10:06:53 +0900

    [add] 주석 추가 및 레거시 코드 삭제

--- app/src/main/java/com/example/magnettimer/MainActivity.kt
+++ app/src/main/java/com/example/magnettimer/MainActivity.kt
@@ -5,9 +5,7 @@ import android.content.Intent
 import android.graphics.Color
 import android.nfc.NfcAdapter
 import android.nfc.Tag
-import android.nfc.tech.IsoDep
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -21,30 +19,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // UI 초기화
         window.statusBarColor = Color.parseColor("#050625")
 
         // NFC 어댑터 확인 및 활성화 상태 확인
         nfcAdapter = NfcAdapter.getDefaultAdapter(this)
         if (nfcAdapter == null) {
-            Toast.makeText(this, "이 기기에서는 NFC가 지원되지 않습니다.", Toast.LENGTH_SHORT).show()
+            // NFC가 지원되지 않는 경우
+            showToast("이 기기에서는 NFC가 지원되지 않습니다.")
             return
         }
 
-
         if (!nfcAdapter!!.isEnabled) {
-            Toast.makeText(this, "NFC가 활성화되어 있지 않습니다.", Toast.LENGTH_SHORT).show()
-            return
+            // NFC가 비활성화된 경우
+            showToast("NFC가 활성화되어 있지 않습니다.")
         } else {
-            Toast.makeText(this, "NFC가 활성화 되었습니다.", Toast.LENGTH_SHORT).show()
+            // NFC가 활성화된 경우
+            showToast("NFC가 활성화 되었습니다.")
         }
 
         // 버튼 클릭 이벤트 처리
         val startButton = findViewById<Button>(R.id.timeStart)
         startButton.setOnClickListener {
             // NFC 전방향 디스패치 활성화
-            nfcAdapter?.enableForegroundDispatch(this, nfcPendingIntent, null, null)
+            enableNfcForegroundDispatch()
 
-            // ActivityTimerOnd로 이동
+            // 타이머 액티비티로 이동
             val intent = Intent(this, timer_on::class.java)
             startActivity(intent)
         }
@@ -65,7 +65,9 @@ class MainActivity : AppCompatActivity() {
 
     // onPause 시 NFC 전방향 디스패치 비활성화
     override fun onPause() {
-        super.onPause() // 타이머 정지
+        super.onPause()
+
+        // 포그라운드 디스패치 비활성화 (타이머 정지)
         nfcAdapter?.disableForegroundDispatch(this)
     }
 
@@ -75,6 +77,7 @@ class MainActivity : AppCompatActivity() {
         handleNfcIntent(intent)
     }
 
+    // NFC 태그 처리
     private fun handleNfcIntent(intent: Intent) {
         val action: String? = intent.action
         if (NfcAdapter.ACTION_TAG_DISCOVERED == action || NfcAdapter.ACTION_TECH_DISCOVERED == action) {
@@ -82,7 +85,7 @@ class MainActivity : AppCompatActivity() {
             val tag: Tag? = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG)
             if (tag != null) {
                 // 여기에서 태그 관련 작업 수행
-                Toast.makeText(this, "NFC 태그가 감지되었습니다.", Toast.LENGTH_SHORT).show()
+                showToast("NFC 태그가 감지되었습니다.")
 
                 // 여기에 스톱워치 시작 코드를 추가하세요.
                 // 예를 들어, startStopwatch() 또는 타이머 시작하는 관련 코드를 넣으면 됩니다.
@@ -90,4 +93,13 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // 토스트 메시지 표시
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    // NFC 전방향 디스패치 활성화
+    private fun enableNfcForegroundDispatch() {
+        nfcAdapter?.enableForegroundDispatch(this, nfcPendingIntent, null, null)
+    }
 }
--- app/src/main/java/com/example/magnettimer/splash.kt
+++ app/src/main/java/com/example/magnettimer/splash.kt
@@ -2,27 +2,35 @@ package com.example.MagnetTimer
 
 import android.content.Intent
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 
 class splash : AppCompatActivity() {
+
+    // 지연 시간 상수
+    companion object {
+        private const val DURATION: Long = 1500
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // 레이아웃 설정
         setContentView(R.layout.activity_splash)
+
+        // 상태 표시줄 색상 설정
         window.statusBarColor = Color.parseColor("#050625")
+
+        // 일정 시간 지난 후 메인 액티비티로 이동
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
             startActivity(intent)
             finish()
-        },DURATION)
-
-    }
-    companion object {
-        private const val DURATION : Long = 1500
+        }, DURATION)
     }
 
+    // 뒤로가기 버튼 동작 무시
     override fun onBackPressed() {
         super.onBackPressed()
     }
